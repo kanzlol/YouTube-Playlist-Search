3c99133a58b393332bdcd72697ee0ceda2c6c378
weedwizard420xXx <ankj94@hotmail.com>
2021-10-04 18:01:34 +0200

    List updates without reload
    Doesn't work like shit now :)

--- background.js
+++ background.js
@@ -1,233 +1,39 @@
-const API_KEY = 'YOUR_API_KEY';
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-let nextPageToken;
-let url;
-let formattedPlaylistId;
-let titleList = [];
-let videoLinks = [];
 
-chrome.runtime.onMessage.addListener((message) => {
+chrome.tabs.onActivated.addListener(function(activeInfo) {
 
-  if(message.popupOpen) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) { 
 
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
-      url = tabs[0].url;
-      console.log(url);
-      let currentUrl = url.match(/[&?]list=([^&]+)/i);
-      
-      
-      if(currentUrl[1]) {
-        
-        if(titleList.length == 0) {
-          chrome.storage.local.set({
-            list: []
-          }, function () {
-            console.log("List stored 1");
-          });
-        }
-        
-        chrome.storage.local.get(
-          ['list', 'url'],
-          function(data) {
-            console.log(data.url, currentUrl[1])
-            if(data.url != currentUrl[1] || data.list.length != titleList.length || data.list.length == 0) {
-              
-              titleList = [];
-              videoLinks = [];
-              getPlaylist(url);
-              
-            }
-            else {
-              
-              chrome.extension.onConnect.addListener(function(port) {
-                port.postMessage({msg: "resultList", titleList, videoLinks});
-              });
-              
-            }
-            
-          });
-  
-          chrome.storage.local.set({
-            url: currentUrl[1]
-          });
-          
-      }
-  
-    });
+    url = tabs[0].url;
+    let currentUrl = url.match(/[&?]list=([^&]+)/i);
 
-  }
-
-});
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
-//     url = tabs[0].url;
-//     console.log(url);
-//     let currentUrl = url.match(/[&?]list=([^&]+)/i);
-    
-    
-//     if(currentUrl[1]) {
-      
-//       if(titleList.length == 0) {
-//         chrome.storage.local.set({
-//           list: []
-//         }, function () {
-//           console.log("List stored 1");
-//         });
-//       }
-      
-//       chrome.storage.local.get(
-//         ['list', 'url'],
-//         function(data) {
-//           console.log(data.url, currentUrl[1])
-//           if(data.url != currentUrl[1] || data.list.length != titleList.length || data.list.length == 0) {
-            
-//             titleList = [];
-//             videoLinks = [];
-//             getPlaylist(url);
-            
-//           }
-//           else {
-            
-//             chrome.extension.onConnect.addListener(function(port) {
-//               port.postMessage({msg: "resultList", titleList, videoLinks});
-//             });
-            
-//           }
-          
-//         });
-
-//         chrome.storage.local.set({
-//           url: currentUrl[1]
-//         });
-        
-//     }
-
-//   });
-// });
-
-function onGAPILoad() {
-
-  gapi.client.init({
-    apiKey: API_KEY,
-    discoveryDocs: DISCOVERY_DOCS,
-  }).then(function () {
-
-    console.log('gapi initialized');
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-      gapi.auth.setToken({
-        'access_token': token,
-      });
-
-    });
-  }, function(error) {
-
-    console.log('error', error);
+    if(currentUrl[1]) {
+      chrome.pageAction.show(activeInfo.tabId);
+    }
+    else {
+      chrome.pageAction.hide(activeInfo.tabId);
+    }
 
   });
 
-}
 
-function getPlaylist(url) {
+});
 
-  let playlistId = url;
-  let tempId = playlistId.match(/[&?]list=([^&]+)/i);
-  formattedPlaylistId = tempId[1];
+chrome.tabs.onUpdated.addListener(function(activeInfo) {
+  console.log(activeInfo)
 
-  return gapi.client.youtube.playlistItems.list({
-    "part": "snippet",
-    "maxResults": 50,
-    "playlistId": formattedPlaylistId
-  }).then(function(response) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) { 
 
-    // chrome.extension.onConnect.addListener(function(port) {
-    //   port.postMessage({msg: "loading"});
-    // });
+    url = tabs[0].url;
+    let currentUrl = url.match(/[&?]list=([^&]+)/i);
 
-    console.log("Response", response);
+    if(currentUrl[1]) {
+      chrome.pageAction.show(activeInfo);
+    }
+    else {
+      chrome.pageAction.hide(activeInfo);
+    }
 
-    let playlistItems = response.result.items;
-    playlistItems.forEach(item => {
-      titleList.push(item.snippet.title);
-      videoLinks.push("https://www.youtube.com/watch?v=" + item.snippet.resourceId.videoId + "&list=" + formattedPlaylistId);
-    });
+  });
 
-    requestPlaylist(formattedPlaylistId, response.result.nextPageToken);
 
-  })
-           
-}
-
-function requestPlaylist(playlistId, pageToken) {
-  console.log('page token: ', pageToken)
-
-  let requestOptions = {
-    playlistId: playlistId,
-    part: "snippet",
-    maxResults: 50
-  }
-
-  if(pageToken) {
-
-    requestOptions.pageToken = pageToken;
-
-    return gapi.client.youtube.playlistItems.list(requestOptions)
-      .then(function(response) {
-  
-        console.log(response);
-        nextPageToken = response.result.nextPageToken;
-  
-        let playlistItems = response.result.items;
-  
-  
-        if(playlistItems) {
-  
-          playlistItems.forEach(item => {
-            titleList.push(item.snippet.title);
-            videoLinks.push("https://www.youtube.com/watch?v=" + item.snippet.resourceId.videoId + "&list=" + formattedPlaylistId);
-          });
-  
-        }
-  
-        if(pageToken) {
-  
-          nextPageToken = response.result.nextPageToken
-  
-          if(nextPageToken) {
-  
-            return requestPlaylist(playlistId, nextPageToken);
-  
-          }
-          else {
-  
-            console.log(titleList);
-            chrome.extension.onConnect.addListener(function(port) {
-              port.postMessage({msg: "resultList", titleList, videoLinks});
-            });
-            
-            chrome.storage.local.set({
-              list: titleList
-            });
-            
-  
-          }
-  
-        }
-  
-      });
-
-  }
-  else {
-
-    console.log(titleList);
-    chrome.extension.onConnect.addListener(function(port) {
-      port.postMessage({msg: "resultList", titleList, videoLinks});
-    });
-
-    chrome.storage.local.set({
-      list: titleList
-    });
-
-  }
-
-}
+});
--- popup.js
+++ popup.js
@@ -1,60 +1,219 @@
-let port = chrome.extension.connect({
-  name: "Sample Communication"
-});
+const API_KEY = 'YOUR_API_KEY';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+let nextPageToken;
+let url;
+let formattedUrl;
+let formattedPlaylistId;
+let titleList = [];
+let videoLinks = [];
 
-chrome.runtime.sendMessage({popupOpen: true});
+window.onload = function() {
+  onGAPILoad();
+}
 
-port.onMessage.addListener(function(message) {
-  console.log(message);
+function onGAPILoad() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+
+    console.log('gapi initialized');
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      gapi.auth.setToken({
+        'access_token': token,
+      });
+      stuff();
+
+    });
+  }, function(error) {
+
+    console.log('error', error);
+
+  });
+
+}
+
+
+function stuff() {
+
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+    url = tabs[0].url;
+    console.log(url);
+    let currentUrl = url.match(/[&?]list=([^&]+)/i);
+    formattedUrl = currentUrl[1];
+    
+    
+    if(formattedUrl) {
+      
+      chrome.storage.local.get(
+        ['titles', 'videos', 'url'],
+        function(data) {
+          console.log(data.url, formattedUrl)
+          if(data.url != formattedUrl || data.url != '') {
+            
+            titleList = [];
+            videoLinks = [];
+            getPlaylist(url);
+            
+          }
+          else {
+            console.log("Same playlist")
+            updateList(data.titles, data.videos);
+            
+          }
+          
+        });
+  
+        chrome.storage.local.set({
+          url: formattedUrl
+        });
+        
+    }
+  
+  });
+
+}
+
+function getPlaylist(url) {
+
+  let playlistId = url;
+  let tempId = playlistId.match(/[&?]list=([^&]+)/i);
+  formattedPlaylistId = tempId[1];
+
+  console.log('loading')
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', '#');
+  a.textContent = "Loading...";
+  li.appendChild(a);
+
+  document.getElementById('results').appendChild(li);
+
+  return gapi.client.youtube.playlistItems.list({
+    "part": "snippet",
+    "maxResults": 50,
+    "playlistId": formattedPlaylistId
+  }).then(function(response) {
+
+
+    console.log("Response", response);
+
+    let playlistItems = response.result.items;
+    playlistItems.forEach(item => {
+      titleList.push(item.snippet.title);
+      videoLinks.push("https://www.youtube.com/watch?v=" + item.snippet.resourceId.videoId + "&list=" + formattedPlaylistId);
+    });
+
+    requestPlaylist(formattedPlaylistId, response.result.nextPageToken);
+
+  })
+           
+}
+
+function requestPlaylist(playlistId, pageToken) {
+  console.log('page token: ', pageToken)
+
+  let requestOptions = {
+    playlistId: playlistId,
+    part: "snippet",
+    maxResults: 50
+  }
+
+  if(pageToken) {
+
+    requestOptions.pageToken = pageToken;
+
+    return gapi.client.youtube.playlistItems.list(requestOptions)
+      .then(function(response) {
+  
+        console.log(response);
+        nextPageToken = response.result.nextPageToken;
+  
+        let playlistItems = response.result.items;
+  
+  
+        if(playlistItems) {
+  
+          playlistItems.forEach(item => {
+            titleList.push(item.snippet.title);
+            videoLinks.push("https://www.youtube.com/watch?v=" + item.snippet.resourceId.videoId + "&list=" + formattedPlaylistId);
+          });
+  
+        }
+  
+        if(pageToken) {
+  
+          nextPageToken = response.result.nextPageToken
+  
+          if(nextPageToken) {
+  
+            return requestPlaylist(playlistId, nextPageToken);
+  
+          }
+          else {
+  
+            console.log("Every pageToken completed", titleList);
+            updateList(titleList, videoLinks);
+            
+            chrome.storage.local.set({
+              titles: titleList,
+              videos: videoLinks
+            });
+            
+  
+          }
+  
+        }
+  
+      });
+
+  }
+  else {
+
+    console.log("No pageToken", titleList);
+    updateList(titleList, videoLinks);
+
+    chrome.storage.local.set({
+      titles: titleList,
+      videos: videoLinks
+    });
+
+  }
+
+}
+
+function updateList(titles, videos) {
 
   let ul = document.getElementById('results');
 
-  if(message.msg == "loading") {
+  while(ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
 
-    console.log('loading')
+  let titleList = [];
+  let videoLinks = [];
+
+  titleList = titles;
+  videoLinks = videos;
+
+  console.log(titleList)
+  
+  for(let i = 0; i < titleList.length; i++) {
+    
     let li = document.createElement('li');
     let a = document.createElement('a');
-    a.setAttribute('href', '#');
-    a.textContent = "Loading...";
+    a.setAttribute('href', videoLinks[i]);
+    a.textContent = titleList[i];
     li.appendChild(a);
 
     ul.appendChild(li);
+    li.onclick = function() {
+      chrome.tabs.update({active: true, url: a.href});
+    }
 
   }
 
-  if(message.msg == "resultList") {
-
-    while(ul.firstChild) {
-      ul.removeChild(ul.firstChild);
-    }
-
-    let titleList = [];
-    let videoLinks = [];
-
-    titleList = message.titleList;
-    videoLinks = message.videoLinks;
-
-    console.log(titleList)
-    
-    for(let i = 0; i < titleList.length; i++) {
-      
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-      a.setAttribute('href', videoLinks[i]);
-      a.textContent = titleList[i];
-      li.appendChild(a);
-
-      ul.appendChild(li);
-      li.onclick = function() {
-        chrome.tabs.update({active: true, url: a.href});
-      }
-
-    }
-
-    
-  }
-
-});
+}
 
 document.getElementById('exists').addEventListener('keyup', updateValue);
 
